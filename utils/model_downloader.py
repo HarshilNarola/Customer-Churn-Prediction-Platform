0ab730861602b54c013910b595184604b7531058
Harshil Narola <harshilnarola195@gmail.com>
2026-07-09 14:59:03 +0530

    Add automatic model download for deployment

--- utils/model_downloader.py
+++ utils/model_downloader.py
@@ -1,27 +1,39 @@
 import os
+
 import gdown
 
 
 MODEL_PATH = "models/calibrated_best_model.pkl"
 
-FILE_ID = "YOUR_GOOGLE_DRIVE_FILE_ID"
+FILE_ID = "1vGDptoJyUaanbgBRhFTNZ4tfEeQ43en3"
 
 
 def download_model():
+    """
+    Download the calibrated model from Google Drive
+    if it does not already exist.
+    """
 
     if os.path.exists(MODEL_PATH):
+
+        print("Calibrated model already exists.")
+
         return
 
     os.makedirs("models", exist_ok=True)
 
-    url = f"https://drive.google.com/file/d/1vGDptoJyUaanbgBRhFTNZ4tfEeQ43en3/view?usp=drive_link"
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
 
     print("Downloading calibrated model...")
 
     gdown.download(
-        url,
-        MODEL_PATH,
+
+        url=url,
+
+        output=MODEL_PATH,
+
         quiet=False
+
     )
 
-    print("Download completed.")
+    print("Model downloaded successfully.")
